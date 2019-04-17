3cb11b08efa4886c230e4e6613426cb9d77ab6c6
Gilberto Junior <olamundo@gmail.com>
2019-04-17 10:05:01 -0300

    Dealing with Binary File Response

--- src/Middleware/CacheWithVarnish.php
+++ src/Middleware/CacheWithVarnish.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\Varnish\Middleware;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Closure;
 
 class CacheWithVarnish
@@ -9,6 +10,15 @@ class CacheWithVarnish
     public function handle($request, Closure $next, int $cacheTimeInMinutes = null)
     {
         $response = $next($request);
+
+        if ($response instanceof BinaryFileResponse) {
+            $response->headers->set(config('varnish.cacheable_header_name'), '1');
+            $response->headers->set(
+                'Cache-Control', 'public,
+                max-age='. 60 * ($cacheTimeInMinutes ?? config('varnish.cache_time_in_minutes'))
+            );
+            return $response;
+        }
 
         return $response->withHeaders([
             config('varnish.cacheable_header_name') => '1',
